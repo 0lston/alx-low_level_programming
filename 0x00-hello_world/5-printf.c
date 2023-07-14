854f4bab82f5d6f119a3845b71ffdd6225ddfd8e
0lston <figuigfree1@gmail.com>
2023-07-13 17:23:08 -0700

    hello c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - description
+ *Return: 0 in sucess.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ *main - main entrypoint
+ *Return: 0 in sucess.
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sierof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
