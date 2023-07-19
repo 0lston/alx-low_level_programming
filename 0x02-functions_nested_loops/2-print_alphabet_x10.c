9edc4e5d955db66b49ad1e5b84dbbed7a7bfc8f4
0lston <figuigfree1@gmail.com>
2023-07-18 17:51:19 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,9 @@
 
 void print_alphabet_x10(void)
 {
-	int letter;
+	int letter, i;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar (letter);
