460e3b04aaae577942bee2c85796f9dca69752a8
0lston <figuigfree1@gmail.com>
2023-07-18 19:16:09 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,23 +11,22 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			if (i * j > 9)
 			{
 				_putchar((i * j) / 10 + '0');
 				_putchar((i * j) % 10 + '0');
 			}
 			else
-				_putchar(i * j + '0');
-			if (j < 9)
 			{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+				_putchar(i * j + '0');
 			}
-			else
-				_putchar('\n');
-		}
 	}
+		_putchar('\n');
+}
 }
