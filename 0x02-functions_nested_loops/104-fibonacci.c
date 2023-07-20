26a439a74cb4a560238917ba7dea4756ae122df7
0lston <figuigfree1@gmail.com>
2023-07-20 03:23:02 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,17 +8,38 @@
 int main(void)
 {
 	int i;
-	unsigned long fib = 0, a = 0, b = 1;
+	unsigned long fib = 0, a = 0, b = 1, al, ar, bl, br, fib_l, fib_r;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 92; i++)
 	{
 		fib = a + b;
 		a = b;
 		b = fib;
-		if (i < 97)
-			printf("%lu, ", fib);
-		else
-			printf("%lu\n", fib);
+		printf("%lu, ", fib);
 	}
+	/* since unsigned long cant hold any larger numbers, we need to split it*/
+	al = a / 10000000000;
+	ar = a % 10000000000;
+	bl = b / 10000000000;
+	br = b % 10000000000;
+	for (i = 92; i < 98; i++)
+	{
+		fib_r = ar + br;
+		fib_l = al + bl;
+		if (fib_r > 9999999999)
+		{
+			fib_r %= 10000000000;
+			fib_l += 1;
+		}
+		al = bl;
+		ar = br;
+		bl = fib_l;
+		br = fib_r;
+		if (i < 97)
+			printf("%lu%lu, ", fib_l, fib_r);
+		else
+			printf("%lu%lu\n", fib_l, fib_r);
+	}
+
 	return (0);
 }
