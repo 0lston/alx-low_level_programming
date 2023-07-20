c20680ad782b0b55f541e563ada6ebc8fe1e9740
0lston <figuigfree1@gmail.com>
2023-07-19 18:35:45 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long fib = 0, a = 0, b = 1;
+
+	for (i = 0; i < 98; i++)
+	{
+		fib = a + b;
+		a = b;
+		b = fib;
+		if (i < 97)
+			printf("%ld, ", fib);
+		else
+			printf("%ld\n", fib);
+	}
+	return (0);
+}
