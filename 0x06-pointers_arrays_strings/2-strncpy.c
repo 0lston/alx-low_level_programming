2d34b520267f5ec3061ae8ac27ff53140f14cdcb
random <random@gmail.com>
2023-07-27 14:07:11 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (src[i] && i < n)
-		dest[i] = src[i++];
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
 	while (i < n)
 		dest[i++] = '\0';
 
