c6ef35a9750bbecbf43231ad7584e66e646d7918
0lston <figuigfree1@gmail.com>
2023-08-09 03:20:06 +0100

    fixed errors

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,7 +29,7 @@ char *argstostr(int ac, char **av)
 	if (!concat_s)
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
 			concat_s[k++] = av[i][j];
