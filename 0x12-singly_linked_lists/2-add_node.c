38c67439bcdef0ec4c45db05cbf3af65c5e468df
0lston <figuigfree1@gmail.com>
2023-08-25 02:34:25 +0100

    fixed 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <string.h>
 
 /**
  * add_node - Adds a new node at the beginning of a linked list.
@@ -22,7 +21,10 @@ list_t *add_node(list_t **head, const char *str)
 
 	dup = strdup(str);
 	if (!dup)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
 	while (dup[len])
 		len++;
